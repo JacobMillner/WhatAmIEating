60ef3ed590cca8d01404dde08cef9efa9d0980ae
Jacob <jacob.millner@outlook.com>
2016-09-26 16:19:25 -0400

    First couple helper functions for heavylifting and gitignoring the .csv files

--- HeavyLifting.py
+++ HeavyLifting.py
@@ -1,1 +1,21 @@
-#test commit
+#Lib to read/write meals to csv
+import ctypes  #for alert messages
+import easygui
+
+def msgBox(message):
+    easygui.msgbox(message, title="Alert!")
+
+def addMeal(date, name, link):
+    #open() with the 'a' parameter allows you to append to file
+    f = open('meals.csv','a')
+    
+    #TODO: validation
+    #TODO: Check if first row, if not:\n and comma
+    mealRow = "{0}, {1}, {2}".format(date, name, link)
+    
+    f.write(mealRow)
+    f.close()
+
+    msgBox("Meal added!")
+
+    
